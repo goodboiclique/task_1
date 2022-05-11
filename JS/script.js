ae599a80cb53de9b56de5c4e77c512a78384b6e4
Artem <sploditelartem@gmail.com>
2022-05-11 10:49:22 +0300

    Практика 2

--- JS/script.js
+++ JS/script.js
@@ -2,6 +2,7 @@
 
 const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 
+
 const personalMovieDB = {
    count: numberOfFilms,
    movies: {},
@@ -9,15 +10,28 @@ const personalMovieDB = {
    ganers: [],
    privat: false
 };
+for(let i = 0; i < 2; i++){
+   let   a = prompt('Один и последних просмотренных фильмов?'),
+         b = prompt('На сколько оцените его?');
 
-const a = prompt('Один и последних просмотренных фильмов?'),
-      b = prompt('На сколько оцените его?'),
-      c = prompt('Один и последних просмотренных фильмов?'),
-      d = prompt('На сколько оцените его?');
+   if (a != null && b != null && a != '' && b != '' && a.length < 50){
+      personalMovieDB.movies[a] = b;
+      console.log('Done');
+   } else{
+      console.log('error');
+      i--;
+   }
+}
 
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+if(personalMovieDB.count < 10){
+   alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count < 30){
+   alert('Вы класический зритель');
+} else if (personalMovieDB.count > 50){
+   alert('Вы киономан');
+} else {
+   alert('Произошла ошибка');
+}
 
 console.log(personalMovieDB);
 
